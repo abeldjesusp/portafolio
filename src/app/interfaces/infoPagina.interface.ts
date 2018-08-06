31b814b8a4731e98c6bfa7eba7367640814947fe
Abel De Jesús <abeldjesusp@gmail.com>
2018-08-05 20:07:46 -0400

    Parte 4

---
+++ src/app/interfaces/infoPagina.interface.ts
@@ -0,0 +1,13 @@
+
+
+export interface infoPagina {
+    titulo?: string;
+    email?: string;
+    nombre_largo?: string;
+    nombre_corto?: string;
+    instagram?: string;
+    twitter?: string;
+    github?: string;
+    linkedin?: string;
+    equipo_trabajo?: any[];
+}
