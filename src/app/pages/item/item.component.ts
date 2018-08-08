a50214ca0fe25de419f4020b25d2d2091d865526
Abel De Jesús <abeldjesusp@gmail.com>
2018-08-07 21:53:35 -0400

    Barra de busqueda terminada

--- src/app/pages/item/item.component.ts
+++ src/app/pages/item/item.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductosService } from '../../services/productos.service';
+import { ProductoInformacion } from '../../interfaces/productoInformacion.interface';
 
 @Component({
   selector: 'app-item',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item.component.css']
 })
 export class ItemComponent implements OnInit {
+  
+  producto: ProductoInformacion;
+  id: string;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              public productosService: ProductosService) { }
 
   ngOnInit() {
+
+    this.route.params
+        .subscribe(parametros => {
+          this.productosService.getProducto(parametros['id'])
+              .subscribe((producto: ProductoInformacion) => {
+                this.producto = producto;
+                this.id = parametros['id'];
+              });
+        });
+
   }
 
 }
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { item } from '../interfaces/item.interface';
+import { resolve } from 'url';
 
 @Injectable({
   providedIn: 'root'
@@ -9,18 +10,53 @@ export class ProductosService {
 
   cargando = true;
   items : item[] = [];
+  itemsFiltrado: item[] = []
 
   constructor(private http: HttpClient) { 
-    this.getProductos();
+    this.cargarProductos();
   }
 
-  private getProductos() {
-    this.http.get('https://mi-portafolio-e327a.firebaseio.com/productos_idx.json')
+  private cargarProductos() {
+    return new Promise((resolve,reject)=>{
+      this.http.get('https://mi-portafolio-e327a.firebaseio.com/productos_idx.json')
              .subscribe( (resp: item[]) => {
-               console.log((resp));
                this.items = resp;
                this.cargando = false;
+               resolve();
              });
-    
-  } 
+    });    
+  }
+
+  public getProducto(id: String){
+    return this.http.get(`https://mi-portafolio-e327a.firebaseio.com/productos/${id}.json`);
+  }
+
+  buscarItem(termino: string){
+
+    if(this.items.length === 0){
+      //cargar productos
+      this.cargarProductos().then(()=>{
+        //Ejecutar despues de tener los productos
+        //Aplicar filtro
+        this.filtrarItems(termino);
+      });
+    }else{
+      //Aplicar filtro
+      this.filtrarItems(termino);
+    }
+  }
+
+  private filtrarItems(termino: string){
+    this.itemsFiltrado = [];
+
+    termino = termino.toLocaleLowerCase();
+
+    this.items.forEach(item =>{
+      const tituloLower = item.titulo.toLocaleLowerCase();
+      if(item.categoria.indexOf(termino) >= 0 || tituloLower.indexOf(termino) >= 0){
+        this.itemsFiltrado.push(item);
+      }
+    });
+  }
+
 }
